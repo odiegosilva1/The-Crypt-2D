e856e28f9d590cbf213f0a8acc641b909910e6e5
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2022-08-19 09:42:27 -0300

    change js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContent('2d');
+    canvas.width = 500;
+    canvas.hight = 500;
+
+    
+});
