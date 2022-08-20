e410cfc8142bd364805ac575e6c6a12e446580a0
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2022-08-19 23:28:29 -0300

    update sprites

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+//import { Player } from "./player";
+
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContent('2d');
-    canvas.width = 500;
-    canvas.hight = 500;
+    const ctx = canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 500;
 
-    
-});
+    class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+        }
+        update(){
+
+        }
+        draw(){
+
+        }
+    }
+
+}); 
---
+++ js/player.js
@@ -0,0 +1,9 @@
+//player's controlers
+class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 100;
+    }
+
+}
